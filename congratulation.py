67ce3115b69deb17c55201b29fe6266c83133f4a
Max Smirnov <masynchin@gmail.com>
2022-06-08 01:14:28 +0300

    Rename template to phrase

--- congratulation.py
+++ congratulation.py
@@ -10,9 +10,9 @@ class Congratulation:
     def print(self, out: TextIO):
         """Печать поздравления."""
         if 11 <= self.seconds <= 19 or self.seconds % 10 in (0, 5, 6, 7, 8, 9):
-            template = f"Поздравляем, вы прошли игру за {self.seconds} секунд!"
+            phrase = f"Поздравляем, вы прошли игру за {self.seconds} секунд!"
         elif self.seconds % 10 in (2, 3, 4):
-            template = f"Поздравляем, вы прошли игру за {self.seconds} секунды!"
+            phrase = f"Поздравляем, вы прошли игру за {self.seconds} секунды!"
         elif self.seconds % 10 == 1:
-            template = f"Поздравляем, вы прошли игру за {self.seconds} секунду!"
-        out.write(f"{template}\n")
+            phrase = f"Поздравляем, вы прошли игру за {self.seconds} секунду!"
+        out.write(f"{phrase}\n")
